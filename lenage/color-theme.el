f78d7706d2c041578ca2361a34df5ee479bd110a
Yuan He <lendage@gmail.com>
2012-07-21 00:49:21 +0800

    clear up config file

--- lenage/color-theme.el
+++
@@ -1,8 +0,0 @@
-;;; Pretty colors
-
-
-(color-theme-initialize)
-(add-to-list 'load-path "~/.emacs.d/vendor/emacs-color-theme-solarized")
-;(color-theme-reset-faces)
-(require 'color-theme-solarized)
-(color-theme-solarized-light)
