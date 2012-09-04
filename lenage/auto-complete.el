187a9db9d4a20fc23d5e1f57a82d1a66bd75be8e
Yuan He <lendage@gmail.com>
2012-09-04 10:37:35 +0800

    auto-complete when typing 2 or more char

--- lenage/auto-complete.el
+++ lenage/auto-complete.el
@@ -1,10 +1,10 @@
 (require 'auto-complete)
 (require 'auto-complete-config)
 (global-auto-complete-mode t)
-(setq ac-auto-start t)
+(setq ac-auto-start 2)
 (setq ac-auto-show-menu nil)
 (setq ac-dwim nil) ; To get pop-ups with docs even if a word is uniquely completed
-(setq ac-ignores '("." "=" "@"))
+;; (setq ac-ignores '("." "=" "@" "[" "]" "(" ")"))
 
 (define-key ac-completing-map (kbd "C-n") 'ac-next)
 (define-key ac-completing-map (kbd "C-p") 'ac-previous)
