0a7b80dcd3ba1754a76434ad200df6b8c87b1177
Yuan He <lendage@gmail.com>
2012-11-29 16:40:33 +0800

    Update submodle and config yasnippet-rails

--- lenage/yasnippet.el
+++ lenage/yasnippet.el
@@ -2,3 +2,9 @@
 ;; (yas/load-directory (expand-file-name "snippets" "~/.emacs.d/"))
 (require 'yasnippet)
 (yas-global-mode 1)
+(require 'dropdown-list)
+(setq yas/prompt-functions '( yas/ido-prompt
+                              yas/dropdown-prompt
+                              yas/completing-prompt))
+(yas/load-directory "~/.emacs.d/vendor/yasnippets-rails/rails-snippets")
+(yas/load-directory "~/.emacs.d/vendor/yasnippets-shoulda")
