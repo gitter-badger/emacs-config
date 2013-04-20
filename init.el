93c7e09d25a099b37222377e075fcfe0db5859db
Yuan He <lendage@gmail.com>
2013-04-21 01:42:04 +0800

    show menubar in emacs and add function to update current filename

--- init.el
+++ init.el
@@ -1,7 +1,8 @@
 (add-to-list 'load-path "~/.emacs.d")
 
 ;; Turn off mouse interface early in startup to avoid momentary display
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+;; (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(unless (display-graphic-p) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
