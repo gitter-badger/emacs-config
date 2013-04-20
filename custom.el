11c13deb7c4ba97809aceb3237a910bd9adee94d
Yuan He <lendage@gmail.com>
2013-04-20 22:05:28 +0800

    Update custom.el file

--- custom.el
+++ custom.el
@@ -9,7 +9,6 @@
  '(bmkp-last-as-first-bookmark-file "~/.emacs.bmk")
  '(custom-safe-themes (quote ("580a24d8f262e9dddbcee7572430ccdb3d9a4088d03e853137c65023d0b04e4d" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "5e1d1564b6a2435a2054aa345e81c89539a72c4cad8536cfe02583e0b7d5e2fa" "6bc195f4f8f9cf1a4b1946a12e33de2b156ce44e384fbc54f1bae5b81e3f5496" "211bb9b24001d066a646809727efb9c9a2665c270c753aa125bace5e899cb523" "967c58175840fcea30b56f2a5a326b232d4939393bed59339d21e46cf4798ecf" "8281168b824a806489ca7d22e60bb15020bf6eecd64c25088c85b3fd806fc341" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "21e157396734468d422b47e4fbfd933a039f36a236e32dc47f1982ae60dc982e" "501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" "d2622a2a2966905a5237b54f35996ca6fda2f79a9253d44793cfe31079e3c92b" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "bf9d5728e674bde6a112979bd830cc90327850aaaf2e6f3cc4654f077146b406" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "dc758223066a28f3c6ef6c42c9136bf4c913ec6d3b710794252dc072a3b92b14" "71efabb175ea1cf5c9768f10dad62bb2606f41d110152f4ace675325d28df8bd" default)))
  '(custom-theme-directory "~/.emacs.d/themes/")
- '(custom-theme-load-path (quote ("~/.emacs.d/elpa/solarized-theme-20130409.1801/" custom-theme-directory t)))
  '(fci-rule-color "#073642")
  '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
  '(highlight-tail-colors (quote (("#073642" . 0) ("#546E00" . 20) ("#00736F" . 30) ("#00629D" . 50) ("#7B6000" . 60) ("#8B2C02" . 70) ("#93115C" . 85) ("#073642" . 100))))
@@ -25,6 +24,4 @@
  ;; If there is more than one, they won't work right.
  '(cursor ((t (:background "SeaGreen4"))))
  '(highlight ((t (:background "#2d4948" :foreground "#e1e1e0"))))
- '(mode-line ((t (:background "#878787" :foreground "#eeeeec"))))
- '(mode-line-inactive ((t (:background "#212931" :foreground "#eeeeec"))))
  '(region ((t (:background "#338f86" :foreground "#e1e1e0")))))
