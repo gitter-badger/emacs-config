50fe446c49920fabd1ec7f39582aed221a07627e
Yuan He <lendage@gmail.com>
2013-05-03 12:18:37 +0800

    Add gitkeep and don't use solriased color

--- lenage/global.el
+++ lenage/global.el
@@ -111,7 +111,7 @@
 ;; (setq auto-save-timeout 15)
 
 ;; color-theme
-(load-theme 'solarized-light t)
+;; (load-theme 'solarized-light t)
 ;; (load-theme 'qsimpleq t)                ;soft and clean white background color-theme
 ;; (load-theme 'zen-and-art t)             ; nice dark theme
 ;; (load-theme 'pastels-on-dark t)
