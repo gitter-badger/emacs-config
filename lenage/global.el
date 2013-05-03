e44220de7f34a75e79f6d0cee208ab5ddba0945c
Yuan He <lendage@gmail.com>
2013-05-03 12:27:36 +0800

    Update globale.el

--- lenage/global.el
+++ lenage/global.el
@@ -16,11 +16,38 @@
         ("melpa"       . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
 
+;; Fetch the list of packages available
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(setq package-list
+      '(
+        ;; Language modes
+        ruby-mode yaml-mode clojure-mode coffee-mode go-mode markdown-mode
+                  lua-mode sass-mode css-mode scss-mode slim-mode
+                  ;; Yasnippet
+                  yasnippet yari yas-jit yasnippet-bundle
+                  ;; Themes
+                  solarized-theme subatomic-theme zen-and-art-theme qsimpleq-theme
+                  ;; Tools
+                  magit full-ack gist col-highlight ace-jump-mode ac-dabbrev
+                  find-file-in-project rinari undo-tree rainbow-mode todotxt
+                  diff-hl expand-region diminish
+                  ;; Fun with Emacs
+                  nyan-mode tea-time keyfreq
+                  ;; auto-complete dependenes
+                  popup
+                  ))
+
+;; install the missing packages
+(dolist (package package-list)
+  (when (not (package-installed-p package))
+    (package-install package)))
+
 ;; Use command as the meta key
 ;; (setq ns-command-modifier  'meta)
 
 ;; Don't show the startup screen
-(setq inhibit-startup-message t)
 (setq user-mail-address "lendage@gmail.com")
 ;; "y or n" instead of "yes or no"
 (fset 'yes-or-no-p 'y-or-n-p)
@@ -36,6 +63,7 @@
 
 ;;; Highlight current line
 (global-hl-line-mode 1)
+(global-diff-hl-mode 1)
 
 ;; Add newline at end of files
 (setq require-final-newline t)
@@ -85,7 +113,7 @@
 ;; tab width
 (setq-default tab-width 2)
 (setq js-indent-level 2)
-(setq c-basic-offset 4)
+(setq c-basic-offset 2)
 (setq css-indent-offset 2)
 (setq sh-basic-offset 2)
 (setq-default indent-tabs-mode nil)
@@ -95,24 +123,59 @@
 (setq redisplay-dont-pause t)
 
 (delete-selection-mode t)
-(scroll-bar-mode 0)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(tooltip-mode -1)
+(set-fringe-style 4)
+(tooltip-mode 1)
 (electric-pair-mode t)
 (icomplete-mode 1)
-;; Display time and data in status bar
+;; Display time and date in status bar
 (setq display-time-24hr-format t)
-(display-time)
+(setq display-time-mode 1)
+(setq size-indication-mode 1)
 
-;; auto-save mode on
-;; (setq auto-save-default t)
-;; (setq auto-save-interval 30)
-;; (setq auto-save-timeout 15)
+;; Global undo tree
+;; C-x u show undotree visualize
+(global-undo-tree-mode)
+
+;; ;; Global auto-revert mode when file changes
+(global-auto-revert-mode 1)
+
+;;;; auto-save mode on
+(setq auto-save-default t)
 
 ;; color-theme
 ;; (load-theme 'solarized-light t)
+(load-theme 'misterioso t)
 ;; (load-theme 'qsimpleq t)                ;soft and clean white background color-theme
 ;; (load-theme 'zen-and-art t)             ; nice dark theme
 ;; (load-theme 'pastels-on-dark t)
 ;; (load-theme 'subatomic t)
+
+;; see M-/ in binding.el
+(setq hippie-expand-try-functions-list '(try-expand-dabbrev
+                                         try-expand-dabbrev-all-buffers
+                                         try-expand-dabbrev-from-kill
+                                         try-complete-file-name-partially
+                                         try-complete-file-name
+                                         try-expand-all-abbrevs
+                                         try-expand-list try-expand-line
+                                         try-complete-lisp-symbol-partially
+                                         try-complete-lisp-symbol))
+
+;;  todotxt config
+(require 'todotxt)
+(setq todotxt-file "~/.todo/todo.txt")
+(global-set-key (kbd "C-x t") 'todotxt)
+
+;; powerline
+;; (require 'powerline)
+;; (powerline-default-theme)
+
+;; Tea Time
+(require 'tea-time)
+(setq tea-time-sound "~/Music/notifications/Shots-letsgo.m4a")
+(setq tea-time-sound-command "afplay %s")
+
+;; Diminish
+(require 'diminish)
+(diminish 'undo-tree-mode)
+(diminish 'yas/minor-mode)
