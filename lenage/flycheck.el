1bb4e4fcf54b09a7df65cb2fbd44a514d60c7768
Yuan He <lendage@gmail.com>
2013-09-10 17:56:45 +0800

    Update flycheck and webjump

--- lenage/flycheck.el
+++ lenage/flycheck.el
@@ -1,9 +1,4 @@
 ;;; Flycheck
 
-(defface flymake-errline '((t (:background "#330000" :overline "red" :underline "red"))) nil)
-(defface flymake-warnline '((t (:background "#333300" :overline "yellow" :underline "yellow"))) nil)
-
 ;; enable Flycheck mode in all buffers, in which it can be used
 (add-hook 'after-init-hook #'global-flycheck-mode)
-;; (set-face-background 'flymake-errline "red4")
-;; (set-face-background 'flymake-warnline "dark slate blue")
